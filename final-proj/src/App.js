611490f36bc9f761ba9ad0f9358a10dc916fc563
joyceeqq <joyce041144@gmail.com>
2022-07-07 00:43:44 +0800

    Linked Backend and front end

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -1,15 +1,25 @@
-import React,{ Component }  from 'react';
-
-class App extends Component {
-
-  render() {
-    return (
-       <div>
-         <h1>Hello, Learner.Welcome to our website.</h1>
-       </div>
-
-    );
-  }
-}
-
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+import Navbar from "./components/navbar.js";
+import RecordList from "./components/recordList.js";
+import Edit from "./components/edit.js";
+import Create from "./components/create.js";
+ 
+const App = () => {
+ return (
+   <div>
+     <Navbar />
+     <Routes>
+       <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} />
+       <Route path="/create" element={<Create />} />
+     </Routes>
+   </div>
+ );
+};
+ 
 export default App;
