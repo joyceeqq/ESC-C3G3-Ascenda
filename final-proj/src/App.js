5c929c24134159aa152b58ec987c1b9c199d04c3
Shaun Neo <shaun.neo@gmail.com>
2022-07-10 17:45:15 +0800

    Update import names

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
- 
+
 // We use Route in order to define the different routes of our application
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
- 
+import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+
 // We import all the components we need in our app
 import Navbar from "./components/navbar.js";
 import RecordList from "./components/recordList.js";
 import Edit from "./components/edit.js";
 import Create from "./components/create.js";
-import AuthLayout from "layouts/Auth.js";
- 
+import AuthLayout from "./layouts/Auth.js";
+
 const App = () => {
  return (
    <div>
     <BrowserRouter>
-    <Switch>
+    <Routes>
       <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-    </Switch>
+    </Routes>
   </BrowserRouter>
    </div>
  );
 };
- 
+
 export default App;
--- final-proj/src/index.js
+++ final-proj/src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // import "./assets/plugins/nucleo/css/nucleo.css";
 // //import "@fortawesome/fontawesome-free/css/all.min.css";
 // import "./assets/scss/argon-dashboard-react.scss";
 
-import AuthLayout from "./layouts/Auth.js";
-
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
