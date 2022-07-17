e8db7b27f5fb166fb054b8e0263da3786a94f1e5
Shaun Neo <shaun.neo@gmail.com>
2022-07-17 22:56:44 +0800

    modal popup, need to display resp card company

--- final-proj/src/components/pages/redeem.js
+++ final-proj/src/components/pages/redeem.js
@@ -1,71 +1,114 @@
-import React, { useEffect, useState } from 'react'
-import { Card, Row, Col, Container, Button } from "react-bootstrap";
+import React, {useState } from 'react'
+import { Card, Row, Col, Container, Button, Modal, Form, ModalDialog } from "react-bootstrap";
 
 const CardDetails = () => {
-    const cardInfo = [
-      {
-        image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
-        companyName: "Barclays",
-        exchangeRate: "1400  points to 400  miles",
-        exchangeTime:"4  to  6  hours",
-        minAmount:"1400 points",
-      },
-      {
-        image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
-        companyName: "Barclays",
-        exchangeRate: "1400  points to 400  miles",
-        exchangeTime:"4  to  6  hours",
-        minAmount:"1400 points",
-      },
-      {
-        image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
-        companyName: "Barclays",
-        exchangeRate: "1400  points to 400  miles",
-        exchangeTime:"4  to  6  hours",
-        minAmount:"1400 points",
-      },
-      {
-        image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
-        companyName: "Barclays",
-        exchangeRate: "1400  points to 400  miles",
-        exchangeTime:"4  to  6  hours",
-        minAmount:"1400 points",
-      },
-      {
-        image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
-        companyName: "Barclays",
-        exchangeRate: "1400  points to 400  miles",
-        exchangeTime:"4  to  6  hours",
-        minAmount:"1400 points",
-      },
-      {
-        image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
-        companyName: "Barclays",
-        exchangeRate: "1400  points to 400  miles",
-        exchangeTime:"4  to  6  hours",
-        minAmount:"1400 points",
-      },
-    ];
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [cardCompanyName, setCardCompanyName] = useState("Barclays");
+
+  const ModalContent = () => {
     return (
-        <Container>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{cardCompanyName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Membership#</Form.Label>
+              <Form.Control
+                type="text"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Confirm Membership#</Form.Label>
+              <Form.Control
+                type="text"
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Save Membership
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+
+  const cardInfo = [
+    {
+      image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
+      companyName: "Barclays",
+      exchangeRate: "1400  points to 400  miles",
+      exchangeTime:"4  to  6  hours",
+      minAmount:"1400 points",
+    },
+    {
+      image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
+      companyName: "Barclays",
+      exchangeRate: "1400  points to 400  miles",
+      exchangeTime:"4  to  6  hours",
+      minAmount:"1400 points",
+    },
+    {
+      image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
+      companyName: "Barclays",
+      exchangeRate: "1400  points to 400  miles",
+      exchangeTime:"4  to  6  hours",
+      minAmount:"1400 points",
+    },
+    {
+      image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
+      companyName: "Barclays",
+      exchangeRate: "1400  points to 400  miles",
+      exchangeTime:"4  to  6  hours",
+      minAmount:"1400 points",
+    },
+    {
+      image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
+      companyName: "Barclays",
+      exchangeRate: "1400  points to 400  miles",
+      exchangeTime:"4  to  6  hours",
+      minAmount:"1400 points",
+    },
+    {
+      image: "https://images.prismic.io/ascendaloyaltycorp/74725b6f-2f69-41d4-94a2-67c4ea3cf01c_Ascenda_Clients_Barclays.png?auto=compress,format",
+      companyName: "Barclays",
+      exchangeRate: "1400  points to 400  miles",
+      exchangeTime:"4  to  6  hours",
+      minAmount:"1400 points",
+    },
+  ];
+    return (
+      <>
+        <div>
             <Row>
                 {cardInfo.map((card, k) => (
                     <Col key={k} xs={12} md={4} lg={3}>
                         <Card >
                             <Card.Img src={card.image} />
-
                             <Card.Body>
                                 <Card.Title>{card.companyName}</Card.Title>
                                 <Card.Text>Exchange Rate: {card.exchangeRate}</Card.Text>
                                 <Card.Text>Estimated Transfer Time: {card.exchangeTime}</Card.Text>
                                 <Card.Text>Minimum Exchange Amount: {card.minAmount}</Card.Text>
-                                <Button variant="primary">Transfer</Button>
+                                <Button variant="primary" onClick={handleShow}>Transfer</Button>
                             </Card.Body>
                         </Card>
                     </Col>
                 ))}
             </Row>
-        </Container>
+            {show ? <ModalContent/> : null}
+        </div>
+        
+      </>
     )
 }
 
