b2c3382c381ccf1cf48091b2bca75620c4dab425
joyceeqq <joyce041144@gmail.com>
2022-08-11 16:24:21 +0800

    Imported notif package

--- frontend/src/views/pages/Tables.js
+++ frontend/src/views/pages/Tables.js
@@ -9,6 +9,8 @@ import {
 // core components
 import Header from "./../../components/Headers/Header.js";
 import React, {useState, useEffect} from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Tables = () => {
 
