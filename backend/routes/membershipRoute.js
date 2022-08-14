7fa5565efba5d9f470a3f6196fa218854b85536c
joyceeqq <joyce041144@gmail.com>
2022-08-14 20:05:26 +0800

    final edits

--- backend/routes/membershipRoute.js
+++ backend/routes/membershipRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const transferReq = require("./../Model/transactionDetailModel");
 const program = require("./../Model/programModel");
-const programDet = require("./../Model/programDetailModel");
 const transactionDet = require("./../Model/transactionDetailModel");
 const {jsonToCSV, createHeader, createFileName} = require("../Functions/generateTransactionCSV");
 const {connectToSFTP, putDataToSFTP, getDataFromSFTP, getRecentHandbackFileName} = require("../sftp");
